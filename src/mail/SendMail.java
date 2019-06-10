046bf242520047ebed04edf2de2ee1cc0d9cde85
Sekar & Arif <>
2019-06-10 16:13:27 +0700

    [sekar, Arif] Email View

--- src/mail/SendMail.java
+++ src/mail/SendMail.java
@@ -10,59 +10,63 @@ package mail;
  * @author Sekar Ayu Safitri
  */
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Session;
 import javax.mail.Message;
 import javax.mail.Transport;
 import javax.mail.Authenticator;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.internet.InternetAddress;
 import javax.mail.PasswordAuthentication;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author Sekar Ayu Safitri
  */
-class SendMail {
+public class SendMail {
 
-     public static void send(String to, String sub,String msg, final String user, final String pass) 
-    {
+    public static void send(String to, String sub, String msg, final String user, final String pass) {
         Properties props = new Properties();
 
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");	
+        props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        
-        Session session = Session.getDefaultInstance(props,new Authenticator() 
-        {
+
+        Session session = Session.getDefaultInstance(props, new Authenticator() {
             @Override
-            protected PasswordAuthentication getPasswordAuthentication() 
-            {
+            protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(user, pass);
             }
         });
 
-        try 
-        {
+        try {
             Message message = new MimeMessage(session);
-            
+
             message.setFrom(new InternetAddress(user));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(sub);
             message.setText(msg);
 
+            DataSource source = new FileDataSource("C:/Users/Sekar Ayu Safitri/Documents/jenius.jpeg");
+            message.setDataHandler(new DataHandler(source));
+            message.setFileName("hayo"); 
+
             Transport.send(message);
-            
-            JOptionPane.showMessageDialog(null,"Email sended!");
-            
-        } catch (MessagingException e) 
-        {
-            JOptionPane.showMessageDialog(null,"Something happened!");
-            
+
+            JOptionPane.showMessageDialog(null, "Email sended!");
+
+        } catch (MessagingException e) {
+            JOptionPane.showMessageDialog(null, "Something happened!");
+
             throw new RuntimeException(e);
         }
     }
 }
-
